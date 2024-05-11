58c0b83b5944247e64f0d6d89aa996cf02ee94b5
AWEsome5199 <awhitcherestrada@hotmail.com>
2024-05-11 11:38:31 +0100

    Delete submit.php

--- submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET["email"])) {
-    $email = $_GET["email"];
-    file_put_contents("mail.txt", $email . PHP_EOL, FILE_APPEND | LOCK_EX);
-    echo "Email added successfully!";
-} else {
-    echo "No email provided!";
-}
-?>
